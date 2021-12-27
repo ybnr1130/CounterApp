222dd9751a19b5f1d2f5cea7da083ef06e965425
ybnr1130 <muuckk@gmail.com>
2021-12-28 07:17:18 +0900

    first commit

---
+++ CounterApp/CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by 薮内紀仁 on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
