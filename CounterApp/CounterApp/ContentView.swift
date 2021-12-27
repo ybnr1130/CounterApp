b441a4567adf8cda373a1cca3e39f0b5bafa2756
ybnr1130 <muuckk@gmail.com>
2021-12-28 07:25:40 +0900

    Make counter button

--- CounterApp/CounterApp/ContentView.swift
+++ CounterApp/CounterApp/ContentView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(number)")
+            Button(action: {self.number += 1}) {
+                Text("カウント")
+            }
+        }
     }
 }
 
